4abcd8c50623e1baa41d055e2eb03bf574a85f05
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-05-04 11:42:50 -0400

    Time: 38 ms (96.97%), Space: 18.1 MB (26.56%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -10,46 +10,20 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        element_dict = dict()
         
-        if not root:
+        if (not root) or (not root.left) :
             return root
         
-        element_queue = []
-        
-        element_queue.append((root, 0))
-        
-        while element_queue:
-            element_queue_member = element_queue.pop(0)
-            
-            if element_queue_member[1] in element_dict:
-                current_level_element_list = element_dict[element_queue_member[1]]
-                current_level_element_list.append(element_queue_member[0])
-                
-                element_dict[element_queue_member[1]] = current_level_element_list
-            else:
-                element_dict[element_queue_member[1]] = [element_queue_member[0]]
-            
-            
-            element_queue_member_left_child = element_queue_member[0].left
-            element_queue_member_right_child = element_queue_member[0].right
-            
-            element_queue_member_child_level = element_queue_member[1] + 1
-            
-            if element_queue_member_left_child:
-                element_queue.append((element_queue_member_left_child, element_queue_member_child_level))
-            
-            if element_queue_member_right_child:
-                element_queue.append((element_queue_member_right_child, element_queue_member_child_level))
-                
+        root.left.next = root.right
         
         
-        for key in element_dict:
-            current_key_list = element_dict[key]
-            
-            for i in range(0, len(current_key_list) - 1):
-                current_key_list[i].next = current_key_list[i + 1]
-            
+        if root.next:
+            root.right.next = root.next.left
+        self.connect(root.left)
+        self.connect(root.right)
+        
+        
         
         return root
+        
         
