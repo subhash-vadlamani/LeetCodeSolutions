60f25a15a8a04978b5f30a28086c803196753070
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-05-25 15:10:27 -0400

    Time: 113 ms (87.45%), Space: 17.6 MB (99.74%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,46 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows, cols = len(board), len(board[0])
+
+        # Step 1: convert the unsurrounded regions into T
+
+        def dfs(r, c):
+            if (r < 0 or r > rows - 1) or (c < 0 or c > cols - 1) or (board[r][c] != "O"):
+                return
+            
+            board[r][c] = "T"
+            dfs(r, c+1)
+            dfs(r, c-1)
+            dfs(r-1, c)
+            dfs(r+1, c)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if (r == 0 or r == rows - 1 or c == 0 or c == cols - 1) and board[r][c] == "O":
+                    dfs(r, c)
+        
+
+
+        # Step 2: the 'O's that still remin are surrounded. So 'O' -> 'X'
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+        
+
+        # Step 3: The 'T's should be converted back to 'O's
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "T":
+                    board[r][c] = "O"
+
+
+        
+
+        
+        
