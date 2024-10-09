e3a604a9355d8e16c2135b66ffeede461208d6de
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-10-09 19:49:12 -0400

    Time: 30 ms (83.17%), Space: 16.4 MB (66.42%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+
+        def construct_dp(n):
+            dp = [0] * (n + 1)
+            dp[0] = 0
+            dp[1] = 1
+            dp[2] = 1
+
+            for i in range(3, n + 1):
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+            return dp
+        
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        else:
+            dp = construct_dp(n)
+            return dp[n]
+            
+
+        
