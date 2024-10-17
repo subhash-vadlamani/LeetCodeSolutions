153d2e1e47e065ebb980151f9a43e4e119a48658
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-10-16 22:27:46 -0400

    Time: 24 ms (98.92%), Space: 16.5 MB (68.55%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.py
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -1,65 +1,48 @@
 import heapq
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        heap = []
-        for i, freq in enumerate([a,b,c]):
-            if freq > 0:
-                heapq.heappush(heap, (-freq, chr(i + ord('a'))))
-        heapq.heapify(heap)
-        ans = []
-        while heap and len(heap) > 1:
-            add_later = []
-            val,char = heapq.heappop(heap)
-            val2,char2 = heapq.heappop(heap)
 
-            val = -val
-            val2 = -val2
-            if val == 1:
-                ans.append(char)
-                val -= 1
+        res, maxHeap = "", []
+        for count, char in [(-a, "a"), (-b, "b"), (-c, "c")]:
+            if count != 0:
+                heapq.heappush(maxHeap, (count, char))
+        
+        while maxHeap:
+            count, char = heapq.heappop(maxHeap)
+            if len(res) > 1 and res[-1] == res[-2] == char:
+                if not maxHeap:
+                    break
+                count2, char2 = heapq.heappop(maxHeap)
+                res += char2
+                count2 += 1
+                if count2:
+                    heapq.heappush(maxHeap, (count2, char2))
             else:
-                if (ans and char != ans[-1]) or not ans:
-                    ans.append(char*2)
-                    val -= 2
-                else:
-                    ans.append(char)
-                    val -= 1
-            if val2 >= 1:
-                ans.append(char2)
-                val2 -= 1
-            if val > 0:
-                add_later.append((-val,char))
-            if val2 > 0:
-                add_later.append((-val2,char2))
-            for v,ch in add_later:
-                heapq.heappush(heap, (v,ch))
-        if heap and -heap[0][0] > 0:
-            val,ch = heap[0]
-            val = -val
-            if val == 1:
-                ans.append(ch)
-            else:
-                ans.append(ch*2)
-        return "".join(ans)
+                res += char
+                count += 1
+            if count:
+                heapq.heappush(maxHeap, (count, char))
+        return res
+
         # pq = []
-        # pq.heappush('a', -a)
-        # pq.heappush('b', -b)
-        # pq.heappush('c', -c)
+        # heapq.heappush(pq, ( -a, 'a'))
+        # heapq.heappush(pq, (-b, 'b'))
+        # heapq.heappush(pq, (-c, 'c'))
 
         # longest_possible_happy_string = ""
 
         # def heap_push_elements(pq, element_list):
         #     for element in element_list:
-        #         pq.heappush()
+        #         heapq.heappush(pq, element)
 
         # while True:
         #     current_pq_list = []
         #     while pq:
-        #         current_pq_list.append(pq.heappop())
+        #         current_pq_list.append(heapq.heappop(pq))
             
         #     if len(longest_possible_happy_string) < 2:
-        #         character_to_append = current_pq_list[0][0]
-        #         character_to_append_count = current_pq_list[0][1] * -1
+        #         character_to_append = current_pq_list[0][1]
+        #         character_to_append_count = current_pq_list[0][0] * -1
 
         #         longest_possible_happy_string += character_to_append
         #         character_to_append_count -= 1
@@ -67,7 +50,21 @@ class Solution:
         #         if character_to_append_count == 0:
         #             current_pq_list.pop(0)
         #         else:
-        #             current_pq_list[0] == (character_to_append, character_to_append_count * -1)
+        #             current_pq_list[0] == (character_to_append_count * -1, character_to_append)
+        #     else:
+        #         character_to_append = current_pq_list[0][1]
+        #         character_to_append_count = current_pq_list[0][0] * -1
+
+        #         last_two_characters = longest_possible_happy_string[-2:]
+        #         if last_two_characters == character_to_append + character_to_append:
+        #             if len(current_pq_list) == 1:
+        #                 return longest_possible_happy_string
+        #             else:
+        #                 character_to_append = currrent_pq_list[1][1]
+        #                 character_to_append_count = current_pq_list[1][0] * -1
+
+
+
 
 
 
