5032b3097acd1c0e6b4696f7ee674448bcca77dd
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-10-21 20:30:31 -0400

    Time: 159 ms (89.46%), Space: 16.6 MB (20.85%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -1,22 +1,22 @@
 class Solution:
     def maxUniqueSplit(self, s: str) -> int:
-        seen = set()
-        max_count = [0]
-        self.backtrack(s, 0, seen, 0, max_count)
-        return max_count[0]
 
-    def backtrack(
-        self, s: str, start: int, seen: set, count: int, max_count: list
-    ) -> None:
-        if count + (len(s) - start) <= max_count[0]:
-            return
-        if start == len(s):
-            max_count[0] = max(max_count[0], count)
-            return
-        for end in range(start + 1, len(s) + 1):
-            sub_string = s[start:end]
-            if sub_string not in seen:
-                seen.add(sub_string)
-                self.backtrack(s, end, seen, count + 1, max_count)
-                seen.remove(sub_string)
-        return
+        def dfs(i, cur_set):
+            if i == len(s):
+                return 0
+            
+            res = 0
+
+            for j in range(i, len(s)):
+                substr = s[i:j+1]
+
+                if substr in cur_set:
+                    continue
+                cur_set.add(substr)
+                res = max(res, 1 + dfs(j + 1, cur_set))
+                cur_set.remove(substr)
+
+            return res
+        
+        return dfs(0, set())
+        
