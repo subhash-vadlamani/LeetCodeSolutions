2207d467b6ffea98aee6a735d717345c0140a3df
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-11-02 16:29:13 -0400

    Time: 683 ms (91.49%), Space: 17.1 MB (45.92%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,36 +1,22 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        minimum_coins_list = [-2] * (amount + 1)
-        minimum_coins_list[0] = 0
 
-        def compute_minimum_coins(amount):
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
 
-            if amount < 0:
-                return -1
-
-            if minimum_coins_list[amount] != -2:
-                return minimum_coins_list[amount]
-            
-            current_min = -1
-            for coin in coins:
-                remaining_amount = amount - coin
-                if remaining_amount < 0 or minimum_coins_list[remaining_amount] == -1:
+        for a in range(1, amount + 1):
+            min_coins = -1
+            for c in coins:
+                remaining_amount = a - c
+                if remaining_amount < 0 or dp[remaining_amount] == -1:
                     continue
                 
-                current_coins = 1 + compute_minimum_coins(remaining_amount)
-                if current_coins == 0:
-                    continue
-                
-                if current_min == -1:
-                    current_min = current_coins
+                if min_coins == -1:
+                    min_coins = 1 + dp[remaining_amount]
                 else:
-                    current_min = min(current_min, current_coins)
-            
-            minimum_coins_list[amount] = current_min
-            return current_min
+                    min_coins = min(min_coins, 1 + dp[remaining_amount])
+            dp[a] = min_coins
         
-        compute_minimum_coins(amount)
-        return minimum_coins_list[amount]
+        return dp[amount]
                 
-
         
