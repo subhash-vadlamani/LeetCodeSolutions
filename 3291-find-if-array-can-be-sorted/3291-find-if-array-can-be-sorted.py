81447ff01500e03595840ce5fa7721fbfa419ce0
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-11-06 21:52:24 -0500

    Time: 126 ms (5.31%), Space: 16.8 MB (23.11%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.py
@@ -1,64 +1,14 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-        """
-            Write a method that takes the list of elements
-            and outputs a dict of numbers and count of set bits for 
-            those numbers
-        """
-
-        def countSetBits(binary_rep):
-            set_bits_count = 0
-
-            for c in binary_rep:
-                if c == '1':
-                    set_bits_count += 1
-            return set_bits_count
-        def createSetBitsDict(nums):
-            set_bits_dict = dict()
-            for num in nums:
-                binary_rep = bin(num)[2:]
-                if num not in set_bits_dict:
-                    binary_rep = str(bin(num)[2:])
-                    set_bits_dict[num] = countSetBits(binary_rep)
-            return set_bits_dict
         
-        set_bits_dict = createSetBitsDict(nums)
+        N = len(nums)
 
-        """
-            Splitting the array into segments where each segment has same number of 
-            set bits
-        """
-
-        segment_list = []
-        previous_set_bits = set_bits_dict[nums[0]]
-        previous_list = [nums[0]]
-        n = len(nums)
-        for i in range(1, n):
-            current_set_bits = set_bits_dict[nums[i]]
-
-            if current_set_bits == previous_set_bits:
-                previous_list.append(nums[i])
-            else:
-                previous_set_bits = current_set_bits
-                segment_list.append(previous_list)
-                previous_list = [nums[i]]
-        
-        segment_list.append(previous_list)
-
-        if len(segment_list) == 1:
-            return True
-        
-        for i in range(1, len(segment_list)):
-            prev_segment_elements = segment_list[i-1]
-            current_segment_elements = segment_list[i]
-
-            if not(max(prev_segment_elements) < min(current_segment_elements)):
+        for _ in range(N):
+            for j in range(N-1, 0, -1):
+                if nums[j] < nums[j-1] and nums[j].bit_count() == nums[j-1].bit_count():
+                    nums[j], nums[j-1] = nums[j-1], nums[j]
+                
+        for i in range(N - 1):
+            if nums[i] > nums[i + 1]:
                 return False
         return True
-            
-        
-
-
-
-
-        
