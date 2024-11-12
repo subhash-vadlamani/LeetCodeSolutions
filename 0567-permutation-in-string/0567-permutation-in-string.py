8926d701a4d8ccaf31d80437a505708177a86317
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-11-12 17:16:55 -0500

    Time: 15 ms (86.19%), Space: 16.7 MB (37.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,70 +1,54 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
 
-        def compute_character_dict_list(s, required_substring_length):
-            """
-                Compute all the substrings of the required length and also
-                store the mapping of the count of each and every character
-                in a dictionary.
-            """
-
-            """
-                First, we compute all the substrings of the given length
-            """
-
-            i = 0
-            j = required_substring_length
-            given_string_length = len(s)
-            substring_list = []
-
-            while j <= given_string_length:
-                current_substring = s[i:j]
-                substring_list.append(current_substring)
-                i += 1
-                j += 1
-            
-            """
-                Now that we have the list of substrings, we make a 
-                list of dicts where each dict contains the count of 
-                english characters of each substring
-            """
-
-            character_dict_list = []
-            for substring in substring_list:
-                current_character_dict = dict()
-
-                for char in substring:
-                    if char not in current_character_dict:
-                        current_character_dict[char] = 1
-                    else:
-                        current_character_dict[char] += 1
-                character_dict_list.append(current_character_dict)
-            
-            return character_dict_list
-        
-
         """
-            We have to make sure that if the s1 length is more than that of s2
-            we have to return false
+            Sliding Window Technique
         """
 
-        if len(s1) > len(s2):
+        s1_len = len(s1)
+        s2_len = len(s2)
+
+        if s1_len > s2_len:
             return False
-        main_string_char_dict = dict()
-        required_substring_length = len(s1)
+        
+        s1_dict = dict()
+        s2_dict = dict()
 
         for char in s1:
-            if char not in main_string_char_dict:
-                main_string_char_dict[char] = 1
+            if char not in s1_dict:
+                s1_dict[char] = 1
             else:
-                main_string_char_dict[char] += 1
+                s1_dict[char] += 1
         
 
-        character_dict_list = compute_character_dict_list(s2, required_substring_length)
+        l = 0
+        r = s1_len
 
-        for current_character_dict in character_dict_list:
-            if current_character_dict == main_string_char_dict:
+        for char in s2[l:r]:
+
+            if char not in s2_dict:
+                s2_dict[char] = 1
+            else:
+                s2_dict[char] += 1
+        
+        if s1_dict == s2_dict:
+            return True
+        while r < s2_len:
+            if s2[r] not in s2_dict:
+                s2_dict[s2[r]] = 1
+            else:
+                s2_dict[s2[r]] += 1
+            
+            if s2_dict[s2[l]] == 1:
+                s2_dict.pop(s2[l])
+            else:
+                s2_dict[s2[l]] -= 1
+            
+            if s1_dict == s2_dict:
                 return True
-        return False
+            r += 1
+            l += 1
+        
+        return True if s1_dict == s2_dict else False
 
         
