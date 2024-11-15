2b2b84d7ae6e5827495965314b3b599a4da704b6
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-11-14 21:14:23 -0500

    Time: 31 ms (53.39%), Space: 16.8 MB (54.47%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.py
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.py
@@ -1,33 +1,26 @@
 class Solution:
-    def primeSubOperation(self, nums):
-        max_element = max(nums)
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        def is_prime(n):
+            
+            for f in range(2, int(sqrt(n)) + 1):
+                if n % f == 0:
+                    return False
+            return True
+        
+        prev = 0
+        for n in nums:
+            upper_bound = n - prev # non inclusive
 
-        # Store the sieve array.
-        sieve = [1] * (max_element + 1)
-        sieve[1] = 0
-        for i in range(2, int(math.sqrt(max_element + 1)) + 1):
-            if sieve[i] == 1:
-                for j in range(i * i, max_element + 1, i):
-                    sieve[j] = 0
-
-        # Start by storing the currValue as 1, and the initial index as 0.
-        curr_value = 1
-        i = 0
-        while i < len(nums):
-            # Store the difference needed to make nums[i] equal to currValue.
-            difference = nums[i] - curr_value
-
-            # If difference is less than 0, then nums[i] is already less than
-            # currValue. Return false in this case.
-            if difference < 0:
+            largest_p = 0
+            for i in reversed(range(2, upper_bound)):
+                if is_prime(i):
+                    largest_p = i
+                    break
+            
+            if n - largest_p <= prev:
                 return False
-
-            # If the difference is prime or zero, then nums[i] can be made
-            # equal to currValue.
-            if sieve[difference] or difference == 0:
-                i += 1
-                curr_value += 1
-            else:
-                # Otherwise, try for the next currValue.
-                curr_value += 1
+            
+            prev = n - largest_p
         return True
+
+        
