fed91ad82195b014782c271aa476ec7ecace98b0
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-12-06 16:32:51 -0500

    Time: 34 ms (9.32%), Space: 20.8 MB (37.47%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,49 +1,22 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # start_list = ['0']*26
+        # start_string = "".join(start_list)
+        str_dict = dict()
 
-        my_dict = dict()
+        for current_str in strs:
+            current_key_list = [0] * 26
+            for char in current_str:
+                current_key_list[ord(char) - ord('a')] += 1
+            
+            current_key = "".join(str(current_key_list))
 
-        for s in strs:
-            sorted_s = (sorted(s))
-            sorted_string = ""
-            for char in sorted_s:
-                sorted_string += char
-            # print(sorted_string)
-            if sorted_string not in my_dict:
-                my_dict[sorted_string] = [s]
+            if current_key not in str_dict:
+                str_dict[current_key] = [current_str]
             else:
-                my_dict[sorted_string].append(s)
-        
-        # print(my_dict)
-        answer = list(my_dict.values())
-        return answer
+                str_dict[current_key].append(current_str)
 
-        # def get_tuple_key(s):
-        #     dict_key = dict()
-
-        #     for char in s:
-        #         if char not in dict_key:
-        #             dict_key[char] = 1
-        #         else:
-        #             dict_key[char] += 1
-        #     key_as_tuple = tuple(dict_key.items())
-        #     return key_as_tuple
-        
-        # anagram_dict = dict()
-
-        # for s in strs:
-        #     current_string_tuple_key = get_tuple_key(s)
-
-        #     if current_string_tuple_key not in anagram_dict:
-        #         anagram_dict[current_string_tuple_key] = [s]
-        #     else:
-        #         anagram_dict[current_string_tuple_key].append(s)
-        
-        # answer = list(anagram_dict.values())
-        # return answer
+        return list(str_dict.values())
 
 
-
-
-        
         
