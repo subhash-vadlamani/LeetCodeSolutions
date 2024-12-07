359e9729cb4d7fed2c5f26aee8cd66ab1ebbb1dc
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-12-06 19:18:28 -0500

    Time: 128 ms (71.64%), Space: 77.6 MB (58.83%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,38 +1,49 @@
-from collections import deque
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
 class LRUCache:
 
     def __init__(self, capacity: int):
-        """
-            1. Initialize a queue
-            2. store the capacity
-            3. initialize a key-value store
-        """
-        self.my_dict = dict()
         self.capacity = capacity
-        self.queue = deque()
+        self.cache = {} # map the key to nodes
+
+        # Left = LRU, right = MRU
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next , self.right.prev = self.right, self.left
+        
+    # remove node from the list
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+    
+    # Insert at right
+    def insert(self, node):
+        node.prev, node.next = self.right.prev, self.right
+        self.right.prev, node.prev.next = node, node
         
 
     def get(self, key: int) -> int:
-        if key in self.my_dict:
-            self.queue.remove(key)
-            self.queue.append(key)
-            return self.my_dict[key]
-
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
         return -1
-
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.my_dict:
-            self.queue.remove(key)
-            self.queue.append(key)
-            self.my_dict[key] = value
-        else:
-            if len(self.queue) == self.capacity:
-                popped_key = self.queue.popleft()
-                self.my_dict.pop(popped_key)
-            self.queue.append(key)
-            self.my_dict[key] = value
+        if key in self.cache:
+            self.remove(self.cache[key])
+        
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.capacity:
+            # remove from the list and dlete the LRU from the hashmap
+
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
         
 
 
