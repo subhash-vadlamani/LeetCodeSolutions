421e7cc4c37f0ce493d3a70e3b47ff1077c06951
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-12-17 13:59:31 -0500

    Time: 113 ms (13.18%), Space: 18.2 MB (8.98%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -2,15 +2,27 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        euro_symbol = chr(0x20AC)
-        encoded_string = "\u20AC".join(strs)
+        encoded_string = ""
+        delimiter = "#"
+        for s in strs:
+            encoded_string += str(len(s)) + delimiter + s
+        print(encoded_string)
         return encoded_string
         
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        return s.split("\u20AC")
+        answer_list = []
+        i = 0
+        while i < len(s):
+            current_length = int(s[i:].split('#')[0])
+            current_length_digits = len(str(current_length))
+            current_string = s[(i + current_length_digits + 1):(i + current_length_digits + 1 + current_length)]
+            answer_list.append(current_string)
+            i += current_length + current_length_digits + 1
+        return answer_list
+
         
 
 
