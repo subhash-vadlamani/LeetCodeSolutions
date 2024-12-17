edcf8b91569543d7db0ba72be2c4c3b23741b174
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-12-17 11:26:58 -0500

    Time: 7 ms (54.46%), Space: 21.4 MB (25.92%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,27 @@
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        nums_count_dict = dict()
 
+        # Min Heap and a element count dict
+
+        num_count_dict = dict()
         for num in nums:
-            if num in nums_count_dict:
-                nums_count_dict[num] += 1
+            if num not in num_count_dict:
+                num_count_dict[num] = 1
             else:
-                nums_count_dict[num] = 1
+                num_count_dict[num] += 1
         
-        sorted_keys = sorted(nums_count_dict, key = lambda k:nums_count_dict[k], reverse = True)
-
-        return sorted_keys[:k]
+        heap = []
+        for index, (key, val) in enumerate(num_count_dict.items()):
+            if len(heap) < k:
+                heapq.heappush(heap, [val, key])
+            else:
+                if val > heap[0][0]:
+                    heapq.heappop(heap)
+                    heapq.heappush(heap, [val, key])
+        # print(heap)
+        answer = []
+        for i in range(len(heap)):
+            answer.append(heap[i][1])
+        return answer
         
