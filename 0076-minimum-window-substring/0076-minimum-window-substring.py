dcadb4dc6c352e305f4d01396df0f66e248de73d
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-12-20 20:39:17 -0500

    Time: 63 ms (83.82%), Space: 18.3 MB (6%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,65 +1,34 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        s_len = len(s)
-        t_len = len(t)
-
-        if s_len < t_len:
-            return ""
-
-        t_dict = dict()
-        for i in range(t_len):
-            if t[i] not in t_dict:
-                t_dict[t[i]] = 1
-            else:
-                t_dict[t[i]] += 1
-        s_dict = dict()
-
-        def substring_contains_required_chars(s_dict, t_dict):
-            """
-                Makes sure that for every (key1, val1) pair in t_dict
-                there exists (key2, val2) pair in s_dict
-                such that key1 == key 2 and val2 >= val1
-            """
-
-            for _, (key, val) in enumerate(t_dict.items()):
-                if key not in s_dict or val > s_dict[key]:
-                    return False
-            return True
         
-        minimum_substring_length = float('inf')
-        minimum_substring_indices = (0, 0)
+        if t == "": return ""
 
+        countT, window = {}, {}
+
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+        
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float('inf')
         l = 0
-        r = t_len - 1 
-        for i in range(l, r + 1):
-            if s[i] not in s_dict:
-                s_dict[s[i]] = 1
-            else:
-                s_dict[s[i]] += 1
-        
-        while r < s_len:
-            if substring_contains_required_chars(s_dict, t_dict):
-                current_substring_length = r - l + 1
-                if current_substring_length < minimum_substring_length:
-                    minimum_substring_length = current_substring_length
-                    minimum_substring_indices = (l, r)
-                    if current_substring_length == t_len:
-                        break
-                s_dict[s[l]] -= 1
-                l += 1
-            else:
-                r += 1
-                if r < s_len:
-                    if s[r] not in s_dict:
-                        s_dict[s[r]] = 1
-                    else:
-                        s_dict[s[r]] += 1
-        
-        if minimum_substring_length == float('inf'):
-            return ""
-        else:
-            return s[minimum_substring_indices[0]:minimum_substring_indices[1] + 1]
-        
-            
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
 
+            if c in countT and window[c] == countT[c]:
+                have += 1
+            
+            while have == need:
+                # update our result
+                if (r - l + 1) < resLen:
+                    resLen = r - l + 1
+                    res = [l, r]
+                # pop from the left of our window
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
         
+        l, r = res
+        return s[l:r+1] if resLen != float('inf') else ""
+                
