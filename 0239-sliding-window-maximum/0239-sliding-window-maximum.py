658dd9968d52ce641b0f210c8eba6da04a58016f
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-12-21 12:52:17 -0500

    Time: 179 ms (43.69%), Space: 32 MB (18.54%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,34 +1,24 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        sliding_window_dict = dict()
-        answer = []
-        current_max = float('-inf')
 
-        l = 0
-        for r in range(len(nums)):
-            sliding_window_dict[nums[r]] = 1 + sliding_window_dict.get(nums[r], 0)
+        output = []
+        q = collections.deque() # index
+        l = r = 0
 
-            if r - l + 1 == k:
-                if current_max == float('-inf'):
-                    current_max = max(sliding_window_dict.keys())
-                else:
-                    current_max = max(current_max, nums[r])
+        while r < len(nums):
+            # pop smaller values from q
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
 
-                answer.append(current_max)
-
-                if sliding_window_dict[nums[l]] > 1:
-                    sliding_window_dict[nums[l]] -= 1
-                else:
-                    del sliding_window_dict[nums[l]]
-                    if current_max == nums[l]:
-                        if sliding_window_dict.keys():
-                            current_max = max(sliding_window_dict.keys())
-                        else:
-                            current_max = float('-inf')
-                
+            # remove left val from window
+            if l > q[0]:
+                q.popleft()
+            
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
                 l += 1
-                
-        return answer
-
+            r += 1
+        return output
 
         
