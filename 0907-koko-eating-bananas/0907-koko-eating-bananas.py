5ceddb57bebc341f4b5a52f7b6391a5e9c5f659d
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-12-21 18:19:36 -0500

    Time: 154 ms (52.95%), Space: 19.2 MB (5.57%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,54 +1,18 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
 
-        def can_koko_finish(piles, h, k):
-            # Local copy of piles to avoid mutating original array
-            temp_piles = piles[:]
-            hours_spent = 0
-            for bananas in temp_piles:
-                hours_spent += (bananas + k - 1) // k  # Equivalent to math.ceil(bananas / k)
-                if hours_spent > h:  # If more hours are needed than available
-                    return False
-            return True
-
-        # def can_koko_finish(piles, h, k):
-        #     # k -> koko's banana eating speed
-        #     piles_length = len(piles)
-        #     i = 0
-        #     for j in range(h):
-        #         if i == piles_length:
-        #             return True
-        #         remaining_banans_in_pile = piles[i]
-        #         remaining_banans_in_pile -= k
-        #         if remaining_banans_in_pile <= 0:
-        #             i += 1
-        #         else:
-        #             piles[i] = remaining_banans_in_pile
-        #     if i != piles_length:
-        #         return False
-
-        min_pile_size = float('inf')
-        max_pile_size = float('-inf')
-
-        for i in range(len(piles)):
-            if piles[i] < min_pile_size:
-                min_pile_size = piles[i]
-            
-            if piles[i] > max_pile_size:
-                max_pile_size = piles[i]
-        
-        i = 1
-        j = max_pile_size
-
-        minimum_koko_eating_speed = max_pile_size
-
-        while i <= j:
-            current_koko_eating_speed = (i + j) // 2
-
-            if can_koko_finish(piles, h, current_koko_eating_speed):
-                minimum_koko_eating_speed = current_koko_eating_speed
-                j = current_koko_eating_speed - 1
+        while l <= r:
+            k = (l + r) // 2
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p / k)
+            if hours <= h:
+                res = min(res, k)
+                r = k - 1
             else:
-                i = current_koko_eating_speed + 1
+                l = k + 1
+        return res
+
         
-        return minimum_koko_eating_speed
