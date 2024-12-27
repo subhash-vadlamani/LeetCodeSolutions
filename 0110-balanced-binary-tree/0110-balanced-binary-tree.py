52d23229366b92a7fd1ad448bd86f7f6b77d6e30
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-12-26 20:36:35 -0500

    Time: 7 ms (33.05%), Space: 19 MB (9.31%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -7,20 +7,33 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         
-
+        depth_dict = {}
         def depth(node):
+            nonlocal depth_dict
             if not node:
                 return 0
+            
+            if node in depth_dict:
+                return depth_dict[node]
             
             left_depth = depth(node.left)
             right_depth = depth(node.right)
 
-            return 1 + max(left_depth, right_depth)
-        if not root:
-            return True
-        left_depth = depth(root.left)
-        right_depth = depth(root.right)
+            current_node_depth = 1 + max(left_depth, right_depth)
+            depth_dict[node] = current_node_depth
+            return current_node_depth
         
-        if abs(left_depth - right_depth) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right):
-            return True
-        return False
+        def check_balanced(node):
+            if not node:
+                return True
+            
+            left_depth = depth(node.left)
+            right_depth = depth(node.right)
+
+            if abs(left_depth - right_depth) > 1:
+                return False
+            return check_balanced(node.left) and check_balanced(node.right)
+        
+        depth(root)
+        return check_balanced(root)
+        
