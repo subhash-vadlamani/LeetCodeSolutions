7e5b8c9366d71533c1e1d051d9d7c444e33fe05d
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2024-12-28 21:56:32 -0500

    Time: 13 ms (38.1%), Space: 18.1 MB (7.9%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,35 +1,22 @@
-import copy
 class Solution:
-
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        solutions = []
-        state = []
-        candidates.sort()
-        self.search(state, target, candidates, 0 , solutions)
-        return solutions
+        res = []
 
-    def is_valid_state(self, state, target):
-        # check if it is a valid solution
-        return sum(state) == target
 
-    def get_candidates(self, state, target, candidates):
-        if sum(state) > target:
-            return []
-        else:
-            return candidates
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            
+            if i >= len(candidates) or total > target:
+                return
+            
+            cur.append(candidates[i])
+            dfs(i, cur, total + candidates[i])
 
-    def search(self, state, target, candidates,  start, solutions):
-        if self.is_valid_state(state, target):
-            solutions.append(copy.deepcopy(state))
-            return
-
-        for i in range(start, len(candidates)):
-            candidate = candidates[i]
-            if sum(state) + candidate > target:
-                break
-            state.append(candidate)
-            self.search(state, target, candidates, i, solutions)
-            state.pop()
-
-    
+            cur.pop()
+            dfs(i + 1, cur, total)
+        
+        dfs(0, [], 0)
+        return res
         
