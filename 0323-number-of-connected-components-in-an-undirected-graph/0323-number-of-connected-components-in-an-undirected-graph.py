74ab575849e443cdc01a5a91e43b59200f77aff7
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-01-04 11:18:55 -0500

    Time: 3 ms (91.03%), Space: 19.1 MB (28.32%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,35 +1,33 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        g = defaultdict(list)
-        for a, b in edges:
-            g[a].append(b)
-            g[b].append(a)
-        
-        UNVISITED = 0
-        VISITING = 1
-        VISITED = 2
-        states = [UNVISITED] * n
-        
-        def dfs(i, prev, current_node_number):
-            state = states[i]
-            if state == VISITED:
-                return current_node_number
+        par = [i for i in range(n)]
+        rank = [1] * n
+
+        def find(n1):
+            res = n1
             
-            current_node_number += 1
-            states[i] = VISITED
-
-            for nei in g[i]:
-                if nei != prev:
-                    dfs(nei, i, current_node_number)
-            return current_node_number
+            while res != par[res]:
+                par[res] = par[par[res]]
+                res = par[res]
+            return res
         
-        number_of_components = 0
-        for i in range(n):
-            new_nodes_visited = dfs(i, -1, 0)
-            # print(states)
-            if new_nodes_visited:
-                number_of_components += 1
-        return number_of_components
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
 
-
+            if p1 == p2:
+                return 0
+            
+            if rank[p2] > rank[p1]:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            else:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            
+            return 1
+        
+        res = n
+        for n1, n2 in edges:
+            res -= union(n1, n2)
+        return res
         
