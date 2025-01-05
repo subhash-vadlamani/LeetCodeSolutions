d7ad52119b8bd4aeaee3772d1685d6a067ab6f3f
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-01-05 12:11:24 -0500

    Time: 393 ms (29.1%), Space: 19.8 MB (21.39%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -1,45 +1,28 @@
-import heapq
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
 
-        # Step1: Create an adj list with the given data
+        edges = collections.defaultdict(list)
 
-        g = defaultdict(dict)
         for u, v, w in times:
-            g[u][v] = w
+            edges[u].append((v, w))
         
-        INF = 10 ** 20
-        min_visit_times = [INF] * (n + 1)
-        visit_set = set()
+        minHeap = [(0, k)]
+        visit = set()
+        t = 0
 
-        heap = []
-        # heap will store a tuple (a, b). a -> time to reach node b
+        while minHeap:
+            w1, n1 = heapq.heappop(minHeap)
 
-        heapq.heappush(heap, (0, k))
-        while heap:
-            # break out of the look and return the value if all the nodes have been visited
-            if len(visit_set) == n:
-                return max(min_visit_times[1:])
-
-            # pop the earliest reachable node
-            current_min_time, current_min_time_node = heapq.heappop(heap)
-
-            # update the minimum reachable time for that particular node
-            min_visit_times[current_min_time_node] = min(min_visit_times[current_min_time_node], current_min_time)
-
+            if n1 in visit:
+                continue
             
-            # explore the node if it has not been already explored
-            if current_min_time_node not in visit_set:
-                for v, w in g[current_min_time_node].items():
-                    heapq.heappush(heap, (current_min_time + w, v))
-            
-            visit_set.add(current_min_time_node)
+            visit.add(n1)
+            t = max(t, w1)
+
+            for n2, w2 in edges[n1]:
+                if n2 not in visit:
+                    heapq.heappush(minHeap, (w1 + w2, n2))
         
-        # Return -1 if the length of the visit_set is never equal to n
-
-        if len(visit_set) == n:
-            return max(min_visit_times[1:])
-        else:
-            return -1
+        return t if len(visit) == n else -1
 
         
