4c5ee83af0e907cb4ec686f8c3e7b8f6019f5eeb
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-01-06 13:22:51 -0500

    Time: 44 ms (27.14%), Space: 18.7 MB (7.5%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -1,30 +1,25 @@
-import heapq
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        n = len(grid)
 
+        N = len(grid)
         visit = set()
-        minHeap = [[grid[0][0], (0, 0)]] # [elevation, point]
-        t = 0
-        while minHeap:
-            # Increment the t until the level of water is enough to pop the lowest grid element
-            while t < minHeap[0][0]:
-                t += 1
-            _, (i, j) = heapq.heappop(minHeap)
-            if (i, j) in visit:
-                continue
-            
-            visit.add((i, j))
-            if (n - 1, n - 1) in visit:
+        minH = [[grid[0][0], 0, 0]] # (time/max-height, r, c)
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        visit.add((0,0))
+
+        while minH:
+            t, r, c = heapq.heappop(minH)
+
+            if r == N - 1 and c == N - 1:
                 return t
-            # Explore the neighbours of (i, j)
-            for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                new_i, new_j = i + di, j + dj
-                if new_i in range(n) and new_j in range(n):
-                    heapq.heappush(minHeap, [grid[new_i][new_j], (new_i, new_j)])
-        return t
             
+            for dr, dc in directions:
+                neiR, neiC = r + dr, c + dc
+                if neiR in range(N) and neiC in range(N) and (neiR, neiC) not in visit:
+                    visit.add((neiR, neiC))
 
+                    heapq.heappush(minH, [max(t, grid[neiR][neiC]), neiR, neiC])
+        
 
-
+            
         
