d44c68b696f35d89ee3f2f3916971e09774e7d91
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-01-13 18:38:12 -0500

    Time: 123 ms (66.84%), Space: 18.5 MB (52.19%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,17 +1,28 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
 
-        cache = {}
+        dp = [defaultdict(int) for _ in range(len(nums) + 1)]
 
-        def dfs(i, current_sum):
-            if i == len(nums):
-                if current_sum == target:
-                    return 1
-                return 0
-            if (i, current_sum) in cache:
-                return cache[(i, current_sum)]
+        dp[0][0] = 1 # (0 elements, 0 sum) -> 1 way
+
+        for i in range(len(nums)):
+            for cur_sum, count in dp[i].items():
+                dp[i + 1][cur_sum + nums[i]] += count
+                dp[i + 1][cur_sum - nums[i]] += count
+        return dp[len(nums)][target]
+
+
+        # cache = {}
+
+        # def dfs(i, current_sum):
+        #     if i == len(nums):
+        #         if current_sum == target:
+        #             return 1
+        #         return 0
+        #     if (i, current_sum) in cache:
+        #         return cache[(i, current_sum)]
             
-            cache[(i, current_sum)] = dfs(i + 1, current_sum + nums[i]) + dfs(i + 1, current_sum - nums[i])
-            return cache[(i, current_sum)]
-        return dfs(0, 0)
+        #     cache[(i, current_sum)] = dfs(i + 1, current_sum + nums[i]) + dfs(i + 1, current_sum - nums[i])
+        #     return cache[(i, current_sum)]
+        # return dfs(0, 0)
         
