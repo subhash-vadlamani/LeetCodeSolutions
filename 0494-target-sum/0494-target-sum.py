2735c6e085b783298084ba104d5fe016bb34ec88
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-01-13 18:42:47 -0500

    Time: 111 ms (73.89%), Space: 18 MB (68.48%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,15 +1,28 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
 
-        dp = [defaultdict(int) for _ in range(len(nums) + 1)]
 
-        dp[0][0] = 1 # (0 elements, 0 sum) -> 1 way
+        dp = defaultdict(int)
+
+        dp[0] = 1 # (0 sum) -> 1 way
 
         for i in range(len(nums)):
-            for cur_sum, count in dp[i].items():
-                dp[i + 1][cur_sum + nums[i]] += count
-                dp[i + 1][cur_sum - nums[i]] += count
-        return dp[len(nums)][target]
+            next_dp = defaultdict(int)
+            for cur_sum, count in dp.items():
+                next_dp[cur_sum + nums[i]] += count
+                next_dp[cur_sum - nums[i]] += count
+            dp = next_dp
+        return dp[target]
+
+        # dp = [defaultdict(int) for _ in range(len(nums) + 1)]
+
+        # dp[0][0] = 1 # (0 elements, 0 sum) -> 1 way
+
+        # for i in range(len(nums)):
+        #     for cur_sum, count in dp[i].items():
+        #         dp[i + 1][cur_sum + nums[i]] += count
+        #         dp[i + 1][cur_sum - nums[i]] += count
+        # return dp[len(nums)][target]
 
 
         # cache = {}
