ec0b3319ca3f807d702e2bdc5186d3e2406e91d0
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-01-16 18:18:08 -0500

    Time: 3 ms (75.16%), Space: 19.7 MB (31.66%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,18 +1,14 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort(key = lambda i:i[0])
-
         if not intervals:
             return True
-
-        non_overlapping_intervals = [intervals[0]]
-
+        intervals.sort()
+        prevEnd = intervals[0][1]
         for start, end in intervals[1:]:
-            lastEnd = non_overlapping_intervals[-1][1]
-
-            if start < lastEnd:
+            if start < prevEnd:
                 return False
-            else:
-                non_overlapping_intervals.append([start,end])
+            prevEnd = end
         return True
+
+
         
