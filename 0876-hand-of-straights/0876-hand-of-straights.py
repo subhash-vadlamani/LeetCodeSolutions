f85090c47a858289b3e0b8e394d69b966ac6985f
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-01-15 19:31:14 -0500

    Time: 27 ms (94.77%), Space: 19.1 MB (42.73%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -1,32 +1,61 @@
+import heapq
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        
+        """
+            Use Hashmap to store the numbers.
+            Use Min-Heap to store the minimum
+        """
+
         if len(hand) % groupSize != 0:
             return False
+        number_of_groups = len(hand) // groupSize
+        number_count_dict = dict()
+        for num in hand:
+            if num not in number_count_dict:
+                number_count_dict[num] = 1
+            else:
+                number_count_dict[num] += 1
         
-        # sort the hand list
-        hand.sort()
+        min_heap = []
+        for num in number_count_dict:
+            heapq.heappush(min_heap, num)
 
-        while hand:
-            current_set = set()
-            for i in range(len(hand)):
-                if len(current_set) == 0:
-                    current_set.add(i)
-                    current_num = hand[i]
+        def get_current_minimum_element():
+            # peek at the heap to see what is the minimum element in the heap
+            while min_heap:
+                heap_min = min_heap[0]
+                if heap_min not in number_count_dict:
+                    heapq.heappop(min_heap)
                     continue
+                element_count = number_count_dict[heap_min]
+                if element_count == 1:
+                    required_minimum = heap_min
+                    number_count_dict.pop(heap_min)
+                    heapq.heappop(min_heap)
+                else:
+                    required_minimum = heap_min
+                    number_count_dict[heap_min] -= 1
                 
-                if len(current_set) == groupSize:
-                    break
-                
-                if hand[i] == current_num + 1:
-                    current_set.add(i)
-                    current_num = hand[i]
-            
-            # print("hand - {}. current_set - {}".format(hand, current_set))
-            if len(current_set) != groupSize:
-                return False
+                return required_minimum
+            return -1
 
-            for index in sorted(current_set, reverse = True):
-                hand.pop(index)
+
+
+
+        for i in range(number_of_groups):
+            current_minimum = get_current_minimum_element()
+            if current_minimum == -1:
+                return False
+            for j in range(1, groupSize):
+                if (current_minimum + j) not in number_count_dict:
+                    return False
+                elif number_count_dict[current_minimum + j] == 1:
+                    number_count_dict.pop(current_minimum + j)
+                else:
+                    number_count_dict[current_minimum + j] -= 1
         return True
+
+
+
+        
         
