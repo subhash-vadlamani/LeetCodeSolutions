86db3c67325a3cfd5c71d3bcebeeafebf206219b
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-01-16 12:38:46 -0500

    Time: 7 ms (74.25%), Space: 21.8 MB (7.47%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,39 +1,18 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 
-        intervals.sort(key = lambda i : i[0])
+        # sort the intervals based on the start
+        intervals.sort(key = lambda x:x[0])
+
         output = [intervals[0]]
 
         for start, end in intervals[1:]:
-            # print(output)
-            lastEnd = output[-1][1]
+            last_end = output[-1][1]
 
-            if start <= lastEnd:
-                output[-1][1] = max(lastEnd, end)
+            if start <= last_end:
+                output[-1][1] = max(last_end, end)
             else:
                 output.append([start, end])
-        return output
-
-
-        # if len(intervals) <= 1:
-        #     return intervals
         
-        # sorted_intervals = sorted(intervals, key=lambda x: x[0])
-        # non_overlapping_intervals = []
-
-        # required_start = sorted_intervals[0][0]
-
-        # for i in range(1, len(sorted_intervals)):
-        #     current_start = sorted_intervals[i][0]
-        #     current_end = sorted_intervals[i][1]
-
-        #     previous_start = sorted_intervals[i-1][0]
-        #     previous_end = sorted_intervals[i-1][1]
-
-        #     if current_start > previous_end:
-        #         non_overlapping_intervals.append([required_start, previous_end])
-        #         required_start = current_start
-        # non_overlapping_intervals.append([required_start, max(current_end, previous_end)])
-
-        # return non_overlapping_intervals
+        return output
         
