e70848a86fd70144b7ffda2ab5e1639b2276ba52
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-01-21 15:34:28 -0500

    Time: 3 ms (65.5%), Space: 19 MB (12.39%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,26 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-
-        def sort(nums):
-            i = 0
-            N = len(nums)
-            while(i < N):
-                correct_index = nums[i]
-                if correct_index != i and correct_index != N:
-                    swap(nums, correct_index, i)
-                else:
-                    i += 1
-            return nums
-        
-        def swap(nums, i, j):
-            nums[i], nums[j] = nums[j], nums[i]
-        
-        nums = sort(nums)
-        N = len(nums)
-
-        for i in range(N):
-            if i != nums[i]:
-                return i
-        
-        return N
+        ans = nums[0]
+        ans ^= 0
+        for i in range(1, len(nums)):
+            ans ^= i
+            ans ^= nums[i]
+        return ans ^ len(nums)
         
