8a4f1785f971853b4152ee90c47076d1f7853537
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-02-02 14:17:52 -0500

    Time: 25 ms (68.81%), Space: 29.2 MB (70.09%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,26 +1,22 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
 
-        # Cyclic Sort
+        slow, fast = 0, 0
 
-        """
-            If the value is 'val', the correct index is 'val - 1'
-        """
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
 
-        i = 0
-        nums_len = len(nums)
-
-        while i < nums_len:
-            current_val = nums[i]
-            current_val_correct_index = current_val - 1
-
-            if nums[i] != nums[current_val_correct_index]:
-                nums[i], nums[current_val_correct_index] = nums[current_val_correct_index], nums[i]
-            else:
-                i += 1
+            if slow == fast:
+                break
         
-        for j in range(nums_len):
-            if nums[j] != j + 1:
-                return nums[j]
-        
+        slow2 = 0
+
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+
+            if slow == slow2:
+                return slow
+
         
