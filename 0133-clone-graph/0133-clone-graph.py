a4817b79c6d03a762e427678e618738a4225466c
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-02-02 20:43:33 -0500

    Time: 40 ms (73.09%), Space: 18.1 MB (66.09%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,50 +1,29 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
 from typing import Optional
+class Node:
+  def __init__(self, val =0, neighbors=None):
+    self.val = val
+    self.neighbors = neighbors if neighbors is not None else []
 class Solution:
+  	
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        """
-            store a hashmap where the key is the node value and the value is the node itself
-        """
-        root = node
-        if not root:
-            return root
-        stack = [root]
-        node_dict = {}
-
-        while stack:
-            popped_node = stack.pop()
-            if popped_node.val not in node_dict:
-                new_node = Node(val = popped_node.val)
-                node_dict[popped_node.val] = new_node
-            else:
-                new_node = node_dict[popped_node.val]
-            new_node_neighbors_list = []
-            for neigh in popped_node.neighbors:
-                if neigh.val not in node_dict:
-                    # neighbor not processed before
-                    # neighbor should be added to dict
-
-                    new_neigh_node = Node(neigh.val)
-                    stack.append(neigh)
-                    node_dict[neigh.val] = new_neigh_node
-                else:
-                    new_neigh_node = node_dict[neigh.val]
-                
-                new_node_neighbors_list.append(new_neigh_node)
+        if not node:
+            return None
+      
+        cloned_nodes = {} # Dictionary to store old to new node mappings
+      
+        def dfs(original):
             
-            new_node.neighbors = new_node_neighbors_list
-        
-        return node_dict[node.val]
+            if original in cloned_nodes:
+                return cloned_nodes[original]
             
+            
+            copy  = Node(original.val)
+            cloned_nodes[original] = copy
+            
+            #clone all neighbors
+            for neighbor in original.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+            
+            return copy
 
-
-
-        
-        
+        return dfs(node)
