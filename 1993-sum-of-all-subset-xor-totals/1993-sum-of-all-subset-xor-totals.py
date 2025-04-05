505a96ddd97967b535550a5dfbe6129428169d4a
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-04-05 13:53:06 -0400

    Time: 0 ms (100%), Space: 17.9 MB (23.27%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
@@ -1,33 +1,9 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        subset_list = []
-        nums_length = len(nums)
-
-        def calculate_subset(current_subset, index):
-            if index == nums_length:
-                subset_list.append(current_subset)
-                return
-            
-            calculate_subset(current_subset + [nums[index]], index + 1)
-            calculate_subset(current_subset, index + 1)
+        result = 0
+        N = len(nums)
+        for num in nums:
+            result |= num
         
-        calculate_subset([], 0)
-
-        def calculate_list_xor(my_list):
-            if not my_list:
-                return 0
-            
-            xor_value = my_list[0]
-            for i in range(1, len(my_list)):
-                xor_value ^= my_list[i]
-            return xor_value
-
-        answer = 0
-        # print(subset_list)
-        for subset in subset_list:
-            answer += calculate_list_xor(subset)
-        
-        return answer
-
-
+        return result << (N - 1)
         
