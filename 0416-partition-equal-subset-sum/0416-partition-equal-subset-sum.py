ac0f7f2a364b0d4e5849462b530fc04e254dcdca
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-04-07 14:55:14 -0400

    Time: 1667 ms (23.68%), Space: 114.1 MB (18.34%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,22 +1,43 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2 == 1:
+
+        """
+            Sum of all the elements should be divisible by 2 for this to be possible
+        """
+
+        total_sum = sum(nums)
+        N = len(nums)
+        if total_sum % 2 == 1:
             return False
         
-        dp = set()
-        dp.add(0)
-        target = sum(nums) // 2
-
-        for i in range(len(nums) - 1, -1, -1):
-            nextDP = set()
-            for t in dp:
-                nextDP.add(t + nums[i])
-                nextDP.add(t)
-                if target in nextDP:
-                    return True
-            dp = nextDP
+        cache = {}
         
-        if target in nextDP:
-            return True
-        return False
+        """
+            2 choices for each element. either be in subset 1 or subset 2
+        """
+
+        def compute_subsets(subset1, subset1_sum, subset2, subset2_sum, index):
+
+            """
+                base case 1: 
+                the subset1 sum is equal to total_sum//2
+            """
+            if f"{subset1_sum}, {subset2_sum}, {index}" in cache:
+                return cache[f"{subset1_sum}, {subset2_sum}, {index}"]
+
+            if subset1_sum > total_sum // 2 or subset2_sum > total_sum // 2:
+                return False
             
+            if (subset1_sum == total_sum // 2 or subset2_sum == total_sum // 2) and index == N:
+                return True
+            
+            answer =  (compute_subsets(subset1 + [nums[index]], subset1_sum + nums[index], subset2, subset2_sum, index + 1) 
+            or compute_subsets(subset1, subset1_sum, subset2 + [nums[index]], subset2_sum + nums[index], index + 1))
+            cache[f"{subset1_sum}, {subset2_sum}, {index}"] = answer
+
+            return answer
+        
+        return compute_subsets([], 0, [], 0, 0)
+            
+
+        
