fda82481b9147f6e37469fb5242168af937da8ba
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-04-07 14:58:18 -0400

    Time: 906 ms (43.72%), Space: 156.5 MB (15.13%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,43 +1,21 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-
-        """
-            Sum of all the elements should be divisible by 2 for this to be possible
-        """
-
-        total_sum = sum(nums)
-        N = len(nums)
-        if total_sum % 2 == 1:
+        total = sum(nums)
+        if total % 2 == 1:
             return False
         
-        cache = {}
-        
-        """
-            2 choices for each element. either be in subset 1 or subset 2
-        """
+        target = total // 2
+        memo = {}
 
-        def compute_subsets(subset1, subset1_sum, subset2, subset2_sum, index):
-
-            """
-                base case 1: 
-                the subset1 sum is equal to total_sum//2
-            """
-            if f"{subset1_sum}, {subset2_sum}, {index}" in cache:
-                return cache[f"{subset1_sum}, {subset2_sum}, {index}"]
-
-            if subset1_sum > total_sum // 2 or subset2_sum > total_sum // 2:
-                return False
-            
-            if (subset1_sum == total_sum // 2 or subset2_sum == total_sum // 2) and index == N:
+        def dfs(i, curr_sum):
+            if curr_sum == target:
                 return True
-            
-            answer =  (compute_subsets(subset1 + [nums[index]], subset1_sum + nums[index], subset2, subset2_sum, index + 1) 
-            or compute_subsets(subset1, subset1_sum, subset2 + [nums[index]], subset2_sum + nums[index], index + 1))
-            cache[f"{subset1_sum}, {subset2_sum}, {index}"] = answer
+            if curr_sum > target or i == len(nums):
+                return False
+            if (i, curr_sum) in memo:
+                return memo[(i, curr_sum)]
 
-            return answer
-        
-        return compute_subsets([], 0, [], 0, 0)
-            
+            memo[(i, curr_sum)] = dfs(i + 1, curr_sum + nums[i]) or dfs(i + 1, curr_sum)
+            return memo[(i, curr_sum)]
 
-        
+        return dfs(0, 0)
