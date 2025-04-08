daa7e517e142decbd7d481a4571e3535afece544
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-04-08 14:08:32 -0400

    Time: 23 ms (32.29%), Space: 18 MB (12.6%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,28 +1,29 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        substring_dict = dict()
 
-        l, r = 0, 1
-        len_s = len(s)
-        if len_s == 0:
-            return 0
-        max_len = 1
-        substring_dict[s[0]] = 1
+        """
+            sliding window. 
+            maintain 2 indices i, j
+            maintain longest_length variable to store the length of the longest substring without duplicat
+            characters
 
-        while r < len_s:
-            max_len = max(max_len, r - l)
-            if s[r] in substring_dict:
-                substring_dict.pop(s[l])
-                l += 1
+            maintain seen set
+        """
+
+        i , j = 0, 0
+        longest_length = 0
+        seen = set()
+        s_len = len(s)
+
+        while j < s_len:
+            if s[j] not in seen:
+                seen.add(s[j])
+                current_length = j - i + 1
+                longest_length = max(longest_length, current_length)
+                j += 1
             else:
-                substring_dict[s[r]] = 1
-                r += 1
-        
-        max_len = max(max_len, r - l)
-        
-        return max_len
-            
-
-
+                seen.remove(s[i])
+                i += 1
+        return longest_length
 
         
