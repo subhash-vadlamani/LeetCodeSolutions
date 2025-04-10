56b064fb3e6ead6925dafeb8b1da779e70db1e3c
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-04-09 21:20:46 -0400

    Time: 163 ms (21.2%), Space: 18.1 MB (19.61%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,8 +1,32 @@
+from collections import defaultdict
+import string
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
 
         """
-            Sliding Window Technique
+            Let's break down the problem:
+            1 <= s1.length, s2.length <= 10 ** 4
+
+            Let's think of brute force:
+            1. calculate each and every permutation of the string s1. (TC?)
+            2. Check each and every element in this list and see if it is present in s2. (TC?)
+
+            Let's try and calculate the time complexity of step 1:
+
+            A permutation is defined as a rearrangement of all characters in the string.
+
+        """
+
+        s1_char_freq_dict = defaultdict(int)
+
+        for letter in string.ascii_lowercase:
+            s1_char_freq_dict[letter] = 0
+
+        for current_char in s1:
+            s1_char_freq_dict[current_char] += 1
+        
+        """
+            if len(s1) > len(s2), answer is false
         """
 
         s1_len = len(s1)
@@ -11,44 +35,34 @@ class Solution:
         if s1_len > s2_len:
             return False
         
-        s1_dict = dict()
-        s2_dict = dict()
+        """
+            sliding window technique on s2 with the length of s1_len
+        """
 
-        for char in s1:
-            if char not in s1_dict:
-                s1_dict[char] = 1
-            else:
-                s1_dict[char] += 1
+        # create the initial s2_substring_char_freq_dict
+
+        s2_substring_char_freq_dict = defaultdict(int)
+
+        for letter in string.ascii_lowercase:
+            s2_substring_char_freq_dict[letter] = 0
+            
+        for i in range(0, s1_len):
+            s2_substring_char_freq_dict[s2[i]] += 1
         
-
         l = 0
-        r = s1_len
+        r = s1_len - 1
 
-        for char in s2[l:r]:
-
-            if char not in s2_dict:
-                s2_dict[char] = 1
-            else:
-                s2_dict[char] += 1
-        
-        if s1_dict == s2_dict:
-            return True
-        while r < s2_len:
-            if s2[r] not in s2_dict:
-                s2_dict[s2[r]] = 1
-            else:
-                s2_dict[s2[r]] += 1
-            
-            if s2_dict[s2[l]] == 1:
-                s2_dict.pop(s2[l])
-            else:
-                s2_dict[s2[l]] -= 1
-            
-            if s1_dict == s2_dict:
+        while r < s2_len - 1:
+            if s1_char_freq_dict == s2_substring_char_freq_dict:
                 return True
-            r += 1
+            
+            print(s2_substring_char_freq_dict)
+            s2_substring_char_freq_dict[s2[l]] -= 1
             l += 1
+            r += 1
+            s2_substring_char_freq_dict[s2[r]] += 1
         
-        return True if s1_dict == s2_dict else False
+        print(s2_substring_char_freq_dict)
+        return s1_char_freq_dict == s2_substring_char_freq_dict
 
         
