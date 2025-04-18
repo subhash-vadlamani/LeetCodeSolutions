ea72ca823a137b6fa146b8018abe83e84ec613a9
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-04-18 18:35:47 -0400

    Time: 169 ms (88.79%), Space: 17.8 MB (61.27%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.py
+++ 1656-count-good-triplets/1656-count-good-triplets.py
@@ -1,19 +1,30 @@
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
 
-        def verify_triplet(num1, num2, num3):
-            if abs(num1 - num2) <= a and abs(num2 - num3) <= b and abs(num1 - num3) <= c:
-                return True
-            return False
-        
-
         arr_len = len(arr)
         good_triplet_count = 0
-        for i in range(arr_len - 2):
-            for j in range(i + 1, arr_len - 1):
-                for k in range(j + 1, arr_len):
-                    if verify_triplet(arr[i], arr[j], arr[k]):
-                        good_triplet_count += 1
+        prefix_cnt = [0] * 1001 # prefix counts
+        # prefix_cnt[x] -> count of nums <= x
+        
+        for j in range(arr_len - 1):
+            for k in range(j + 1, arr_len):
+                if abs(arr[j] - arr[k]) <= b:
+                    """
+                        how many values before j
+                        where abs conditions are met
+                    """
+
+                    r = min(arr[j] + a, arr[k] + c)
+                    l = max(arr[j] - a, arr[k] - c)
+
+                    l = max(l, 0)
+                    r = min(r, 1000)
+
+                    if l <= r:
+                        good_triplet_count += prefix_cnt[r] - (0 if l == 0 else prefix_cnt[l - 1])
+            
+            for index in range(arr[j], 1001):
+                prefix_cnt[index] += 1
         
         return good_triplet_count
 
