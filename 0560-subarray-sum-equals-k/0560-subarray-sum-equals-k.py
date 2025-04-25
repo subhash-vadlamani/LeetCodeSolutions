6a6f9f207518abb6dde7defa0e9f82b2384e1c5d
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-04-25 18:42:42 -0400

    Time: 27 ms (84.76%), Space: 20.5 MB (37.83%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,27 +1,20 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
 
-        current_prefix_sum = 0
-        prefix_sum_hashmap = dict()
+        prefix_sum = 0
+        prev_dict = dict()
+        prev_dict[0] = 1
         subarray_count = 0
 
         for i in range(len(nums)):
-            current_prefix_sum += nums[i]
-            if current_prefix_sum == k:
-                subarray_count += 1
-            
-            # current_prefix_sum - required_previous_prefix_sum = k
-            required_previous_prefix_sum = current_prefix_sum - k
+            prefix_sum += nums[i]
 
-            if required_previous_prefix_sum in prefix_sum_hashmap:
-                subarray_count += prefix_sum_hashmap[required_previous_prefix_sum]
-            
-            if current_prefix_sum in prefix_sum_hashmap:
-                prefix_sum_hashmap[current_prefix_sum] += 1
-            else:
-                prefix_sum_hashmap[current_prefix_sum] = 1
+            # we are looking for prev prefix sums of the value (prefix_sum - k)
+
+            subarray_count += prev_dict.get(prefix_sum - k, 0)
+
+            prev_dict[prefix_sum] = 1 + prev_dict.get(prefix_sum, 0)
+        
         return subarray_count
-
-
-
+        
         
