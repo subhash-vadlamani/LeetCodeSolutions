dce7b11bdabc3e8b548b41f46d193cbb19e8be22
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-04-28 17:56:13 -0400

    Time: 0 ms (100%), Space: 17.7 MB (48.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,55 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one = 1
-        two = 1
 
-        for i in range(n - 1):
-            temp = one
-            one += two
-            two = temp
-        return one
+        """
+            DP road map
+
+            Recursion -> Memoization -> True DP
+        """
+
+        # recursive solution -> TLE
+
+        # def dfs(i):
+        #     """
+        #         Number of ways to reach the top from index i
+        #     """
+        #     if i > n:
+        #         return 0
+        #     elif i == n:
+        #         return 1
+        #     else:
+        #         return dfs(i + 1) + dfs(i + 2)
+
+
+        
+        
+        # return dfs(0)
+
+
+        # Memoization
+
+        memo = dict()
+
+        def dfs(i):
+            if i > n:
+                return 0
+            elif i == n:
+                return 1
+            
+            if (i + 1) in memo:
+                val1 = memo[i + 1]
+            else:
+                val1 = dfs(i + 1)
+            
+
+            if (i + 2) in memo:
+                val2 = memo[i + 2]
+            else:
+                val2 = dfs(i + 2)
+            
+            val3 = val1 + val2
+            memo[i] = val3
+            return val3
+        
+        return dfs(0)
         
