74cd14f2453dec36797073f55dc20bc4b9b0b0a1
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-04-28 18:25:25 -0400

    Time: 0 ms (100%), Space: 17.9 MB (30.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -28,28 +28,40 @@ class Solution:
 
         # Memoization
 
-        memo = dict()
+        # memo = dict()
 
-        def dfs(i):
-            if i > n:
-                return 0
-            elif i == n:
-                return 1
+        # def dfs(i):
+        #     if i > n:
+        #         return 0
+        #     elif i == n:
+        #         return 1
             
-            if (i + 1) in memo:
-                val1 = memo[i + 1]
-            else:
-                val1 = dfs(i + 1)
+        #     if (i + 1) in memo:
+        #         val1 = memo[i + 1]
+        #     else:
+        #         val1 = dfs(i + 1)
             
 
-            if (i + 2) in memo:
-                val2 = memo[i + 2]
-            else:
-                val2 = dfs(i + 2)
+        #     if (i + 2) in memo:
+        #         val2 = memo[i + 2]
+        #     else:
+        #         val2 = dfs(i + 2)
             
-            val3 = val1 + val2
-            memo[i] = val3
-            return val3
+        #     val3 = val1 + val2
+        #     memo[i] = val3
+        #     return val3
         
-        return dfs(0)
+        # return dfs(0)
         
+
+        # True DP
+
+        one = 1
+        two = 1
+
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+        
+        return one
