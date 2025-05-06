67f24aaa0e5d8719c925e052e4fba0aeaa269c28
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-05-06 13:50:55 -0400

    Time: 33 ms (5.86%), Space: 19.6 MB (13.03%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.py
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.py
@@ -1,8 +1,19 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        ans = []
+        """
+            convert the list of integers to list of strings 
+        """
 
         for i in range(len(nums)):
-            ans.append(nums[nums[i]])
-        return ans
+            nums[i] = str(nums[i])
+        
+        for i in range(len(nums)):
+            new_num = nums[(int(nums[i].split("-")[0]))]
+            combined_num = nums[i] + "-" + new_num
+            nums[i] = combined_num
+        
+        for i in range(len(nums)):
+            nums[i] = int(nums[i].split("-")[1])
+        
+        return nums
         
