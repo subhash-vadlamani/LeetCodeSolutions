f90f99570cb86ee591039fc67260e71f7c1bd729
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-05-08 17:07:57 -0400

    Time: 138 ms (44.21%), Space: 18.5 MB (23.08%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.py
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.py
@@ -1,30 +1,38 @@
-from typing import List
 import heapq
-
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        n, m = len(moveTime), len(moveTime[0])
-        # dist[i][j] = best‐known earliest arrival at (i,j)
-        dist = [[float('inf')] * m for _ in range(n)]
-        dist[0][0] = 0
+        """
+            solved through using a min-heap. min-heap keeps the 
+            node with the earliest time reached. We can explore that node accordingly.
+            The first occurrance of destination, we return the value stored in the heap
+        """
+        n = len(moveTime)
+        m = len(moveTime[0])
+        min_heap = [(0, (0, 0))] # (time_to_reach_the_cell, cell_coordinates)
+        destination_cell_tuple = (n - 1, m - 1)
+        visit = set()
+        visit.add((0, 0))
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-        heap = [(0, 0, 0)]           # (time, i, j)
-        dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+        while min_heap:
+            current_min_time, current_cell_tuple = heapq.heappop(min_heap)
 
-        while heap:
-            time, i, j = heapq.heappop(heap)
-            if time > dist[i][j]:
-                continue
-            # once we pop the target, it's the minimum possible
-            if i == n-1 and j == m-1:
-                return time
+            if current_cell_tuple == destination_cell_tuple:
+                return current_min_time
+            
+            # visit.add(current_cell_tuple)
+            
+            for dr, dc in directions:
+                new_r = current_cell_tuple[0] + dr
+                new_c = current_cell_tuple[1] + dc
 
-            for di, dj in dirs:
-                ni, nj = i + di, j + dj
-                if 0 <= ni < n and 0 <= nj < m:
-                    arrive = max(time, moveTime[ni][nj]) + 1
-                    if arrive < dist[ni][nj]:
-                        dist[ni][nj] = arrive
-                        heapq.heappush(heap, (arrive, ni, nj))
+                if new_r in range(n) and new_c in range(m) and (new_r, new_c) not in visit:
+                    visit.add((new_r, new_c))
+                    new_cell_time = max(current_min_time, moveTime[new_r][new_c]) + 1
+                    heapq.heappush(min_heap, (new_cell_time, (new_r, new_c)))
+        
+        return -1
+            
 
-        return -1  # unreachable (by problem constraints this shouldn't happen)
+
+        
