6e0d169437c51f7fb596e473fb9948183b4d8bb9
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-06-01 16:01:32 -0400

    Time: 1161 ms (33.1%), Space: 17.6 MB (69.66%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.py
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.py
@@ -1,18 +1,15 @@
-from math import comb
-
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        def count_unbounded_solutions(n):
-            return comb(n + 2, 2) if n >= 0 else 0
 
-        total = count_unbounded_solutions(n)
+        total = 0
 
-        for over1 in range(3):
-            total -= count_unbounded_solutions(n - (limit + 1))
+        for i in range(min(n, limit) + 1):
+            candies_left = n - i
 
-        for over2 in range(3):
-            total += count_unbounded_solutions(n - 2 * (limit + 1))
-
-        total -= count_unbounded_solutions(n - 3 * (limit + 1))
-
+            if limit >= candies_left:
+                total += candies_left + 1
+            else:
+                total += max(limit - (candies_left - limit) + 1, 0)
+        
         return total
+        
