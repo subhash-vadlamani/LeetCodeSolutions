8b562dab19cd47bf60fb727b267e9c9fe590db8b
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-06-02 21:00:28 -0400

    Time: 15 ms (69.29%), Space: 19.9 MB (36.31%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,44 +1,22 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        candies_assigned = {}
-        candies_assigned[0] = 1
-        prev_index = 0
+        """
+            'ratings' list. Each child is assigned a rating value
 
-        for i in range(1, len(ratings)):
+            Greedy method. At each step, try and allocate the least candies as possible.
+
+            
+        """
+        n = len(ratings)
+        answer = [1] * n
+
+        for i in range(1, n):
             if ratings[i] > ratings[i - 1]:
-                current_index_candies = candies_assigned[i-1] + 1
-            else:
-                current_index_candies = 1
-            candies_assigned[i] = current_index_candies
-        for i in range(len(ratings) - 1, -1, -1):
-            if i > 0 and i < len(ratings) - 1:
-                if ratings[i] > ratings[i-1] and ratings[i] > ratings[i+1]:
-                    current_index_candies = max(candies_assigned[i-1], candies_assigned[i+1]) + 1
-                elif ratings[i] > ratings[i-1]:
-                    current_index_candies = candies_assigned[i-1] + 1
-                elif ratings[i] > ratings[i+1]:
-                    current_index_candies = candies_assigned[i+1] + 1
-                else:
-                    current_index_candies = candies_assigned[i]
-                
-                candies_assigned[i] = current_index_candies
-            else:
-                if i == len(ratings) - 1 :
-                    if ratings[i] > ratings[i - 1]:
-                        current_index_candies = candies_assigned[i-1] + 1
-                    else:
-                        current_index_candies = candies_assigned[i]
-                else:
-                    if ratings[i] > ratings[i + 1]:
-                        current_index_candies = candies_assigned[i+1] + 1
-                    else:
-                        current_index_candies = candies_assigned[i]
-                candies_assigned[i] = current_index_candies
+                answer[i] = answer[i - 1] + 1
         
-        total_candies = 0
-
-        for key in candies_assigned:
-            total_candies += candies_assigned[key]
-        return total_candies
-                
-
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                answer[i] = max(answer[i], answer[i+1] + 1)
+        
+        return sum(answer)
+        
