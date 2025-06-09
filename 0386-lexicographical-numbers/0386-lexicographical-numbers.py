3947df32abfee83e07c4e3f6e1fa5e2e9cd68b20
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-06-08 20:14:36 -0400

    Time: 84 ms (14.89%), Space: 22.7 MB (48.35%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,52 +1,22 @@
-from collections import OrderedDict
-class TrieNode:
-    def __init__(self):
-        self.children = OrderedDict()
-        self.is_end = False
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-    
-
-    def insert(self, word_num: int) -> None:
-        node = self.root
-        word = str(word_num)
-        for ch in word:
-            if ch not in node.children:
-                node.children[(ch)] = TrieNode()
-            node = node.children[(ch)]
-        node.is_end = True
-
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        """
-            given an integer n, we have to sort the numbers in the range from [1, n]
 
-            requried T.C -> O(N)
-        """
+        arr = []
 
-        my_trie = Trie()
-
-        for i in range(1, n + 1):
-            my_trie.insert(i)
-        
-
-        answer = []
-        trie_root = my_trie.root
-
-        def dfs(trie_node, prev_key):
-            # perform DFS on the trie and return the answer
-            for key, child_node in trie_node.children.items():
-                if child_node.is_end:
-                    current_key = (prev_key + key)
-                    answer.append(int(current_key))
-                dfs(child_node, current_key)
+        def go(current_num):
             
-            return
+            if current_num <= n:
+                arr.append(current_num)
+            else:
+                return
+            
+            for i in range(10):
+                go(current_num * 10 + i)
+
         
-        dfs(trie_root, "")
-        return answer
+        for i in range(1, 10):
+            go(i)
+        
+        return arr
 
-
-
+        
