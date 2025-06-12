82f94f50482a46e999b1e91560158fe38e68fbb6
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-06-12 13:43:17 -0400

    Time: 1520 ms (33.46%), Space: 25.6 MB (7.32%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,28 +1,21 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-        res = ""
-        resLen = 0
+        s_len = len(s)
+        if s_len == 0:
+            return ""
 
-        for i in range(len(s)):
+        dp = [[False] * s_len for _ in range(s_len)]
+        max_len = 0
+        max_len_string = ""
 
-            # Odd length
-            l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > resLen:
-                    res = s[l:r+1]
-                    resLen = r - l + 1
-                
-                l -= 1
-                r += 1
-            
-            # Even Length
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > resLen:
-                    res = s[l:r+1]
-                    resLen = r - l + 1
-                
-                l -= 1
-                r += 1
-        return res
+        for i in range(s_len - 1, -1, -1):  # reverse order
+            for j in range(i, s_len):  # forward order
+                if s[i] == s[j]:
+                    if j - i < 3 or dp[i + 1][j - 1]:
+                        dp[i][j] = True
+                        current_len = j - i + 1
+                        if current_len > max_len:
+                            max_len = current_len
+                            max_len_string = s[i:j + 1]
+
+        return max_len_string
