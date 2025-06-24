870d00583bef8dc0ad103cfb243b936f67dee615
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-06-24 14:42:50 -0400

    Time: 343 ms (17.35%), Space: 18 MB (68.6%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.py
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.py
@@ -8,13 +8,14 @@ class Solution:
         
         key_index_list = []
         answer_set = set()
-        for i in range(len(nums)):
+        nums_len = len(nums)
+        for i in range(nums_len):
             if nums[i] == key:
                 key_index_list.append(i)
         
         for index in key_index_list:
             for i in range(index - k, index + k + 1):
-                if i in range(len(nums)):
+                if i >= 0 and i < nums_len:
                     answer_set.add(i)
         
         return sorted(list(answer_set))
