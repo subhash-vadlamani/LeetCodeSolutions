612f6ea752f3ec6fd9c60a8346826d0199e3fc1b
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-06-25 13:46:56 -0400

    Time: 365 ms (75.36%), Space: 74.5 MB (40.51%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,28 +1,49 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        memo = {} # (i, j)
+
         s_len = len(s)
         t_len = len(t)
 
-        def dfs(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
-            
-            if j == t_len:
-                return 1
-            
-            if i == s_len:
-                return 0
+        dp = [[0] * (t_len + 1) for _ in range(s_len + 1)]
 
-            if s[i] == t[j]:
-                # we can pick the character at s[i] or not
+        for i in range(s_len + 1):
+            dp[i][t_len] = 1
 
-                count = dfs(i + 1, j + 1) + dfs(i + 1, j)
-            else:
-                count = dfs(i + 1, j)
-            
-            memo[(i, j)] = count
-            return count
+        for i in range(s_len - 1, -1, -1):
+            for j in range(t_len - 1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j]
+                else:
+                    dp[i][j] = dp[i+1][j]
         
-        return dfs(0, 0)
+        return dp[0][0]
+
+
+
+
+        # memo = {} # (i, j)
+        # s_len = len(s)
+        # t_len = len(t)
+
+        # def dfs(i, j):
+        #     if (i, j) in memo:
+        #         return memo[(i, j)]
+            
+        #     if j == t_len:
+        #         return 1
+            
+        #     if i == s_len:
+        #         return 0
+
+        #     if s[i] == t[j]:
+        #         # we can pick the character at s[i] or not
+
+        #         count = dfs(i + 1, j + 1) + dfs(i + 1, j)
+        #     else:
+        #         count = dfs(i + 1, j)
+            
+        #     memo[(i, j)] = count
+        #     return count
+        
+        # return dfs(0, 0)
         
