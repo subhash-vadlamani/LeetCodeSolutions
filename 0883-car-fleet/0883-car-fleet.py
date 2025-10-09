746c721c73cc6c62b7a5ca68ef5d433a2fe0baf3
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-10-09 08:45:03 -0500

    Time: 163 ms (73.49%), Space: 41.1 MB (24.1%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,36 +1,13 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted(zip(position, speed), key = lambda x:-x[0])
+        stack = []
 
-        sorted_data = sorted(zip(position, speed))
-        sorted_position, sorted_speed = zip(*sorted_data)
+        for pos, current_speed in cars:
+            t = (target - pos)/current_speed
 
-        sorted_position = list(sorted_position)
-        sorted_speed = list(sorted_speed)
-
-        sorted_time = []
-        list_length = len(sorted_position)
-
-        for i in range(list_length):
-            current_car_finish_time = ((target - sorted_position[i]) / sorted_speed[i])
-            sorted_time.append(current_car_finish_time)
+            if not stack or t > stack[-1]:
+                stack.append(t)
         
-        # Monotonically increasing stack
-
-        fleet_count = 0
-        current_max_time = float('-inf')
-        for i in range(list_length - 1, -1, -1):
-            if sorted_time[i] > current_max_time:
-                current_max_time = sorted_time[i]
-                fleet_count += 1
-        return fleet_count
-
-
-        # stack = []
-
-        # for i in range(list_length - 1, -1, -1):
-        #     while stack and stack[-1] >= sorted_time[i]:
-        #         stack.pop()
-        #     stack.append(sorted_time[i])
-        # fleet_count = len(stack)
-        # return fleet_count
+        return len(stack)
         
