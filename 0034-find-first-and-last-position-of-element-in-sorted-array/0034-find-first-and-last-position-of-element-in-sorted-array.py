2480083b31fca69fb829c1aad5ffb021e2adcea4
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-10-26 14:30:57 -0400

    Time: 0 ms (100%), Space: 19 MB (52.75%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,31 +1,31 @@
+from typing import List
+
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        if len(nums) == 0:
+        """
+        Use a single binary search function that takes a boolean
+        indicating if we are searching for the leftmost or rightmost index.
+        """
+
+        def perform_search(start, end, searchLeft):
+            result = -1
+            while start <= end:
+                mid = (start + end) // 2
+                if nums[mid] == target:
+                    result = mid
+                    if searchLeft:
+                        end = mid - 1   # keep searching left side
+                    else:
+                        start = mid + 1 # keep searching right side
+                elif nums[mid] < target:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            return result
+
+        if not nums:
             return [-1, -1]
-        
-        
-        left, right = 0, len(nums) - 1
-        
-        while left + 1 < right:
-            mid = left + (right - left) // 2
-            
-            if nums[mid] == target:
-                break
-            elif nums[mid] < target:
-                left = mid
-            else:
-                right = mid
-        
-        
-        
-        while left < len(nums) and nums[left] != target:
-            left += 1
-        while right >= 0 and nums[right] != target :
-            right -= 1
-        
-        if left == len(nums):
-            left = -1
-        
+
+        left = perform_search(0, len(nums) - 1, True)
+        right = perform_search(0, len(nums) - 1, False)
         return [left, right]
-        
