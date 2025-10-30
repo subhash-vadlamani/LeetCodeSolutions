0dde788dae03b073c6992e1edf4e12e126a78fa6
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-10-30 18:18:09 -0400

    Time: 0 ms (100%), Space: 19.6 MB (82.74%) - LeetHub

---
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.py
@@ -0,0 +1,8 @@
+
+class Solution:
+    def closestValue(self, root: TreeNode, target: float) -> int:
+        closest = root.val
+        while root:
+            closest = min(root.val, closest, key = lambda x: (abs(target - x), x))
+            root = root.left if target < root.val else root.right
+        return closest
