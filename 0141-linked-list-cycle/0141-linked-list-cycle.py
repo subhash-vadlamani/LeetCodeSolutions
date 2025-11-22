f2d2bad7a65305a64c41fefac6514dd436526d75
Subhash Vadlamani <39877800+subhash-vadlamani@users.noreply.github.com>
2025-11-22 17:39:44 -0500

    Time: 53 ms (24.04%), Space: 19.8 MB (57.34%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -1,12 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
-        if head is None:
-            return False
-        slow = head
-        fast = head.next
-        while slow != fast:
-            if fast is None or fast.next is None:
-                return False
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # maintain slow and fast pointers
+        """
+            if slow and fast pointers are ever same, cycle exists
+            if fast pointer is null, no cycle exists
+        """
+        slow , fast = head, head
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        return True
+            if slow == fast:
+                return True
+        return False
+        
